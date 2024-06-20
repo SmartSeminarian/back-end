74f316870614edac2e31df14f1dc616b40e3bf77
Kirill Krinkin (box) <kirill@krinkin.com>
2024-06-20 08:25:52 +0300

    added build image

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, redirect, url_for, jsonify
+import os
+from logger import log
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# read version from file if exists
+version = "unknown"
+try:
+    with open("/VERSION") as f:
+        version = f.read()
+except FileNotFoundError:
+    pass
+
+app = Flask(__name__)
+GUNICORN_VERSION=f"{os.getenv('GUNICORN_VERSION', 'Unknown')}"
+log.info('Service started, version: [%s]', GUNICORN_VERSION)
+
+@app.route('/')
+def index():
+    return f"FIX ME! {GUNICORN_VERSION}"
+
+@app.route('/version', methods=['GET'])
+def version():
+    answer = {
+        'api_version': "0.0.1",
+        'build_info' : 'TBD'
+    }
+    return jsonify(answer)
+
