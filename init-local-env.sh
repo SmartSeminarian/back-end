4ab8f964d10c7509659f9122ebae928a7f82b28a
Michalis Christodoulou <michalis.christodoulou@jetbrains.com>
2024-07-11 15:15:42 +0300

    add docker container healthcheck

--- init-local-env.sh
+++ init-local-env.sh
@@ -44,5 +44,9 @@ echo "Version: $BRANCH `hostname` `date`" > $VERSION_FILE
 cat ./.env
 mkdir -p ./.data/
 cp ./deploy/docker-compose.yml ./docker-compose.yml
-sed "s/^#UNCOMMENT_ME_FOR_DEV//" ./deploy/docker-compose.yml > ./docker-compose.yml
+sed -i "s/^#UNCOMMENT_ME_FOR_DEV//" ./docker-compose.yml
+sed -i '0,/5000/s/5000/5000:5000/' ./docker-compose.yml
+sed -i "s/^            test: curl --fail http:\/\/api-stage.csai.site\/version || exit 1//" ./docker-compose.yml
+sed -i "s/^        image: krinkin\/\$PROJECT\.\$SERVICE_NAME:\$BRANCH//" ./docker-compose.yml
+sed -i "/#DELETE_BLOCK_FOR_DEV/,/#DELETE_BLOCK_FOR_DEV/d" ./docker-compose.yml
 
